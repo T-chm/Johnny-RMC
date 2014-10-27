4cdade8020edbb2fdf339a797c330047cc0d7cf9
Hongmin Chen <chmt39@gmail.com>
2014-10-27 19:48:27 +0000

    Add hardware switch visualization

--- webtest.js
+++ webtest.js
@@ -5,6 +5,9 @@ var app = require('http').createServer(handler)
   , five = require("johnny-five"),
   board,servo,led,sensor;
 
+
+var count = 0;
+
 board = new five.Board();
 
 // on board ready
@@ -12,6 +15,15 @@ board.on("ready", function() {
 
   // init a led on pin 13, strobe every 1000ms
   led = new five.Led(13).strobe(1000);
+
+  // init a button
+  this.pinMode(7, five.Pin.INPUT);
+// var state = 0; 
+//	pin.read(function(value) {
+	//console.log(value);
+//	state = value;
+//	console.log(state);
+//	});
 
   // setup a stanard servo, center at start
   servo = new five.Servo({
@@ -21,16 +33,15 @@ board.on("ready", function() {
     center:true
   });
 
-  // poll this sensor every second
-  sensor = new five.Sensor({
-    pin: "A0",
-    freq: 1000
-  });
+sensor = new five.Sensor({
+	pin: "A0",
+	freq: "1000"
+});
 
 });
 
 // make web server listen on port 8000
-app.listen(8000);
+app.listen(9000);
 
 
 // handle web server
@@ -50,24 +61,46 @@ function handler (req, res) {
 
 // on a socket connection
 io.sockets.on('connection', function (socket) {
+  io.set('log level', 1); // reduce logging
+
   socket.emit('news', { hello: 'world' });
  
   // if board is ready
-  if(board.isReady){
+
+if(board.isReady){
     // read in sensor data, pass to browser
-    sensor.on("data",function(){
-      socket.emit('sensor', { raw: this.raw });
-    });
-  }
+console.log("Bello");
+
+var button = new five.Button(5);
+	
+var pin = new five.Pin(7);
+
+button.on("press", function() {
+  console.log( "Button has been pressed" );
+  socket.emit('pin', "OFF");
+});  
+
+  button.on("release", function() {
+    console.log( "Button released" );
+    socket.emit('pin', "ON");  
+});
+
+//	pin.read(function(value){
+//	console.log(value);
+//	socket.emit('pin', value);
+//	});        
+
+}
+
 
   // if servo message received
   socket.on('servo', function (data) {
-    console.log(data);
     if(board.isReady){ servo.to(data.pos);  }
   });
   // if led message received
-  socket.on('led', function (data) {
-    console.log(data);
+ 
+ socket.on('led', function (data) {
+//	console.log(data);
      if(board.isReady){    led.strobe(data.delay); } 
   });
 
