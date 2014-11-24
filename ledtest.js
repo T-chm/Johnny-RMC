445c018267582f582d4623b6276fb75c3e1b370a
Hongmin Chen <chmt39@gmail.com>
2014-11-24 14:14:01 -0500

    added relay functionality

--- ledtest.js
+++ ledtest.js
@@ -1,10 +1,13 @@
-var five = require("johnny-five"),board, led;
+var five = require("johnny-five"),
+    board, led;
 
 board = new five.Board();
 
 board.on("ready", function() {
-  led = new five.Led(13);
-  led.strobe(1000); // on off every second
+    led = new five.Led(13);
+    led.strobe(1000); // on off every second
+    var relay1 = new five.Relay(8);
+
+    relay1.on();
+
 });
-
-
--- webtest.js
+++ webtest.js
@@ -1,107 +1,141 @@
-// initialize everything, web server, socket.io, filesystem, johnny-five
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-  , five = require("johnny-five"),
-  board,servo,led,sensor;
+    // initialize everything, web server, socket.io, filesystem, johnny-five
+    var connect = require('connect'),
+        app = require('http').createServer(handler),
+        io = require('socket.io').listen(app),
+        fs = require('fs'),
+        five = require("johnny-five"),
+        board, servo, led, sensor, relay1, relay2;
 
 
-var count = 0;
+    var count = 0;
 
-board = new five.Board();
+    board = new five.Board();
 
-// on board ready
-board.on("ready", function() {
+    // on board ready
+    board.on("ready", function() {
 
-  // init a led on pin 13, strobe every 1000ms
-  led = new five.Led(13).strobe(1000);
+        // init a led on pin 13, strobe every 1000ms
+        led = new five.Led(13).strobe(1000);
 
-  // init a button
-  this.pinMode(7, five.Pin.INPUT);
-// var state = 0; 
-//	pin.read(function(value) {
-	//console.log(value);
-//	state = value;
-//	console.log(state);
-//	});
+        // var button = new five.Button(5);
 
-  // setup a stanard servo, center at start
-  servo = new five.Servo({
-    pin:6,
-    range: [0,180],
-    type: "standard",
-    center:true
-  });
+        // setup a stanard servo, center at start
+        servo = new five.Servo({
+            pin: 6,
+            range: [0, 180],
+            type: "standard",
+            center: true
+        });
 
-sensor = new five.Sensor({
-	pin: "A0",
-	freq: "1000"
-});
+        sensor = new five.Sensor({
+            pin: "A0",
+            freq: "1000"
+        });
 
-});
-
-// make web server listen on port 8000
-app.listen(9000);
+        relay1 = new five.Relay(8);
+        relay2 = new five.Relay(9);
 
 
-// handle web server
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
+    });
+
+    // make web server listen on port 9000
+    app.listen(9000);
+
+    // handle web server
+
+
+    function handler(req, res) {
+        fs.readFile(__dirname + '/index.html',
+            function(err, data) {
+                if (err) {
+                    res.writeHead(500);
+                    return res.end('Error loading index.html');
+                }
+
+                res.writeHead(200);
+                res.end(data);
+            });
+
+
+
+
     }
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+
+    // on a socket connection
+    io.sockets.on('connection', function(socket) {
+        io.set('log level', 1); // reduce logging
+
+        socket.emit('news', {
+            hello: 'world'
+        });
+
+        // if board is ready
+
+        if (board.isReady) {
+            // read in sensor data, pass to browser
+            console.log("Bello");
 
 
-// on a socket connection
-io.sockets.on('connection', function (socket) {
-  io.set('log level', 1); // reduce logging
+            // var pin = new five.Pin(7);
+            var button = new five.Button(5);
 
-  socket.emit('news', { hello: 'world' });
- 
-  // if board is ready
+            button.on("down", function() {
+                // console.log("Button has been pressed");
+                socket.emit('pin', "OFF");
+            });
 
-if(board.isReady){
-    // read in sensor data, pass to browser
-console.log("Bello");
-
-var button = new five.Button(5);
-	
-var pin = new five.Pin(7);
-
-button.on("press", function() {
-  console.log( "Button has been pressed" );
-  socket.emit('pin', "OFF");
-});  
-
-  button.on("release", function() {
-    console.log( "Button released" );
-    socket.emit('pin', "ON");  
-});
-
-//	pin.read(function(value){
-//	console.log(value);
-//	socket.emit('pin', value);
-//	});        
-
-}
+            button.on("up", function() {
+                // console.log("Button released");
+                socket.emit('pin', "ON");
+            });
 
 
-  // if servo message received
-  socket.on('servo', function (data) {
-    if(board.isReady){ servo.to(data.pos);  }
-  });
-  // if led message received
- 
- socket.on('led', function (data) {
-//	console.log(data);
-     if(board.isReady){    led.strobe(data.delay); } 
-  });
+        }
 
-});
+
+        // if servo message received
+        socket.on('servo', function(data) {
+            if (board.isReady) {
+                servo.to(data.pos);
+            }
+        });
+        // if led message received
+
+        socket.on('led', function(data) {
+            //	console.log(data);
+            if (board.isReady) {
+                led.strobe(data.delay);
+            }
+        });
+
+        socket.on('relay1', function(data) {
+            //	console.log(data);
+            if (board.isReady) {
+                if (data.status) {
+                    relay1.on();
+                }
+
+                if (!data.status) {
+                    relay1.off();
+                }
+            }
+        });
+
+        socket.on('relay2', function(data) {
+            //	console.log(data);
+            if (board.isReady) {
+
+                if (data.status) {
+                    relay2.on();
+                }
+                if (!data.status) {
+                    relay2.off();
+                }
+
+            }
+        });
+
+
+
+    });
